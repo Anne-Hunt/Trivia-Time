02be8e2091fbc3a0629fa9a1f24063035644e1a3
Anne Hunt <acathunt@gmail.com>
2024-04-01 13:52:41 -0600

    Model class created

---
+++ app/models/Example.js
@@ -0,0 +1,13 @@
+
+
+export class Trivia {
+  constructor(data) {
+    this.category = data.category
+    this.question = data.question
+    this.answer = data.correct_answer
+    this.wrong = data.incorrect_answers
+    this.type = data.type
+    this.difficulty = data.difficulty
+  }
+
+}
